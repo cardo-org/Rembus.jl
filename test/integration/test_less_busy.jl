c54bd7c36daa9d84e4ee1f1ea239877edb0cab51
Attilio Donà <attilio.dona@gmail.com>
2024-05-03 18:31:36 +0200

    Test review

--- test/integration/test_less_busy.jl
+++ test/integration/test_less_busy.jl
@@ -38,13 +38,11 @@ function run(client_url, server1_url, server2_url, server3_url)
         @info "[first_call]: result=$res"
         #@test res == 3
     end
-    sleep(0.1)
     @async begin
         res = rpc(client, rpc_topic, request_arg)
         @info "[second_call]: result=$res"
         #@test res == 3
     end
-    sleep(0.1)
     @async begin
         res = rpc(client, rpc_topic, request_arg)
         @info "[third_call]: result=$res"
@@ -57,40 +55,6 @@ function run(client_url, server1_url, server2_url, server3_url)
     @info "[fourth_call]: result=$res"
 
     sleep(2)
-    #@test res == 4
-
-    ##    close(server1)
-    ##
-    ##    res = rpc(client, rpc_topic, request_arg)
-    ##    # server1 down, expect a response from server2
-    ##    @info "[round_robin]: result=$res"
-    ##    @test res == 3
-    ##
-    ##    close(server2)
-    ##    for i in 1:2
-    ##        res = rpc(client, rpc_topic, request_arg)
-    ##        # server1 down, expect a response from server2
-    ##        @info "[round_robin]: result=$res"
-    ##        @test res == 4
-    ##    end
-
-    ##    close(server3)
-    ##    try
-    ##        res = rpc(client, rpc_topic, request_arg)
-    ##    catch e
-    ##        @info "[round_robin]: $e"
-    ##    end
-
-    # no connected exposers
-    ##    @test_throws RpcMethodUnavailable rpc(client, rpc_topic, request_arg)
-    ##
-    ##    server1 = connect(server1_url)
-    ##    expose(server1, rpc_topic, rpc_server1)
-    ##
-    ##    res = rpc(client, rpc_topic, request_arg)
-    ##    @info "[round_robin]: result=$res"
-    ##    @test res == 2
-    ##
     for cli in [client, server1, server2, server3]
         close(cli)
     end
