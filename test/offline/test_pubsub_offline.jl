ab966415a57ef029196519c447827c5962b475e3
Attilio Donà <attilio.dona@gmail.com>
2025-09-18 19:09:41 +0200

    Add test file

---
+++ test/offline/test_pubsub_offline.jl
@@ -0,0 +1,28 @@
+include("../utils.jl")
+
+function mytopic(ctx, rb, val)
+    ctx["count"] += 1
+end
+
+function run()
+    ctx = Dict("count" => 0)
+
+    # First publish n messages
+    pub = component("pub_offline", name="publisher")
+    publish(pub, "mytopic", 1)
+    publish(pub, "mytopic", 2)
+
+    # then subscribe
+    sub = component("sub_offline", name="subscriber")
+    inject(sub, ctx)
+    subscribe(sub, mytopic, Rembus.LastReceived)
+    reactive(sub)
+
+    sleep(0.5)
+    @test ctx["count"] == 2
+
+    close(sub)
+    close(pub)
+end
+
+execute(run, "test_pubsub_offline")
